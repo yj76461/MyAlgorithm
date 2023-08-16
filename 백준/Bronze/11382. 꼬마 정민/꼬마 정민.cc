4f629089eda19c11e2a11859518274220b7d3a02
최윤주 <yj7646@naver.com>
2023-08-16 16:25:11 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long a, b, c;
+    long long result = 0;
+    cin >> a >> b >> c;
+    result = a + b + c;
+    cout << result;
+    
+    return 0;
+}
